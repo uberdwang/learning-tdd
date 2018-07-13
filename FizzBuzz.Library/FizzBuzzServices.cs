03563a11d92d1d9c62fa8a1cf4e016f442deef16
David Wang <dave.wang@janus.com>
2018-07-13 14:33:40 -0600

    Build dotnet sln, lib, and test

---
+++ FizzBuzz.Library/FizzBuzzServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzServices
+    {
+        public string Print(int n) {
+            return string.Empty;
+        }
+    }
+}
